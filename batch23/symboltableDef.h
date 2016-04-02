27fa9634251334360cb93a55e099ccf3c4ec8ca5
dps-codes <deepanshu.singh@gmail.com>
2016-04-02 11:21:59 +0530

    Symbol Table Implementation added

---
+++ batch23/symboltableDef.h
@@ -0,0 +1,34 @@
+#ifndef LEXERDEF
+#define LEXERDEF
+
+struct identifier_list{
+	char *name;
+	int type;
+	struct identifier_list* next;
+};
+
+struct function_node{
+    char *fname;
+    struct identifier_list *input_parameter_list;
+    struct identifier_list *output_parameter_list;
+    struct function_node *next;
+};
+
+//Data structure for Function Hash Table
+struct function_hashtable{
+    int size;
+    struct function_node* table;
+};
+
+typedef struct function_node function_node;
+typedef struct function_hashtable function_hashtable;
+
+//Data structure for storing Identifiers
+struct identifier_hashtbale{
+	int size;
+	struct identifier_list* table;
+};
+typedef struct identifier_list identifier_list;
+typedef struct identifier_hashtable identifier_hashtable;
+
+#endif
