8e18104f5f005bc0ebb5bfa85022689fea0b2500
dps-codes <deepanshu.singh@gmail.com>
2016-04-22 00:42:28 +0530

    Documentation of symboltable complete

--- batch23/symboltable.h
+++ batch23/symboltable.h
@@ -1,27 +1,31 @@
+/*
+BATCH NUMBER: 23
+PRABODH AGARWAL 2012B1A7801P
+DEEPANSHU SINGH 2012B3A7593P
+
+*/
+
 #ifndef SYMBOLTABLE_H
 #define SYMBOLTABLE_H
 function_hashtable* create_function_hashtable(int size);
+identifier_hashtable* create_identifier_hashtable(int size);
+function_wise_identifier_hashtable* create_function_local_identifier_hashtable(int size);
 identifier_list *create_identifier_list(char *name,char *type);
 identifier_list *add_element_to_list(identifier_list *idlist, char *name, char *type);
 identifier_list *addIdentifier(identifier_list *idlist, char *name, char *type, int offset);
-identifier_hashtable* create_identifier_hashtable(int size);
-function_wise_identifier_hashtable* create_function_local_identifier_hashtable(int size);
-void print_function_hashtable(function_hashtable* h);
-void add_identifier_to_hashtable(identifier_hashtable *h, char *name, char *type);
 int add_identifier_to_identifierhashtable(identifier_hashtable *h, char *name, char *type, int offset);
 int add_function_local_identifier_hashtable(function_wise_identifier_hashtable *h, char *fname, identifier_list *idlist, int offset);
 int add_function(function_hashtable* h, char* fname, identifier_list* ip_list, int flag);
-function_node* search_function_hashtable(function_hashtable* h, char *fname);
-identifier_list *get_record_fields(function_wise_identifier_hashtable* h, char *rname);
 int get_record_size(function_wise_identifier_hashtable* h, char *fname);
-identifier_list* get_input_parameter_list(function_hashtable* h, char *fname);
-identifier_list* get_output_parameter_list(function_hashtable* h, char *fname);
+identifier_list* search_global_identifier(identifier_hashtable* h, char *name);
+identifier_list *get_record_fields(function_wise_identifier_hashtable* h, char *rname);
 identifier_hashtable* get_function_identifier_hashtable(function_wise_identifier_hashtable* h, char *fname);
 identifier_list* search_function_wise_identifier_hashtable(function_wise_identifier_hashtable* h, char *fname, char *iname);
+function_node* search_function_hashtable(function_hashtable* h, char *fname);
+identifier_list* get_input_parameter_list(function_hashtable* h, char *fname);
+identifier_list* get_output_parameter_list(function_hashtable* h, char *fname);
 void print_function_hashtable(function_hashtable* h);
 void print_identifier_hashtable(identifier_hashtable *h, char *fname);
 void print_function_wise_identifier_hashtable(function_wise_identifier_hashtable* h);
-void compare_parameter_list_type(identifier_list* i1, identifier_list* i2, char*);
-identifier_list* search_global_identifier(identifier_hashtable* h, char *name);
-
+void compare_parameter_list_type(identifier_list* i1, identifier_list* i2, char* returnmsg);
 #endif
--- batch23/symboltableDef.h
+++ batch23/symboltableDef.h
@@ -1,6 +1,14 @@
+/*
+BATCH NUMBER: 23
+PRABODH AGARWAL 2012B1A7801P
+DEEPANSHU SINGH 2012B3A7593P
+
+*/
+
 #ifndef SYMBOLTABLEDEF
 #define SYMBOLTABLEDEF
 
+//Data structure to store an Identifier
 struct identifier_list{
 	char *name;
 	char *type;
@@ -8,6 +16,7 @@ struct identifier_list{
 	struct identifier_list* next;
 };
 
+//Data Structure to store a Function in Function HashTable
 struct function_node{
     char *fname;
     struct identifier_list *input_parameter_list;
@@ -15,22 +24,20 @@ struct function_node{
     struct function_node *next;
 };
 
-//Data structure for Function Hash Table
+//Data structure for Function HashTable
 struct function_hashtable{
     int size;
     struct function_node** table;
 };
 
-typedef struct function_node function_node;
-typedef struct function_hashtable function_hashtable;
-
-//Data structure for storing Identifiers
+//Data structure for Identifier HashTable
 struct identifier_hashtable{
 	int size;
     int offset;
 	struct identifier_list** table;
 };
 
+//Data Structure to store the address of Function's Identifier HashTable and other details (First Level Hashing)
 struct function_identifier_node{
     char *fname;
     int offset;
@@ -38,11 +45,15 @@ struct function_identifier_node{
     struct identifier_hashtable* id_hashtable;
     struct function_identifier_node *next;
 };
+
+//Data Structure for Function Wise Identifier HashTable
 struct function_wise_identifier_hashtable{
     int size;
     struct function_identifier_node** table;
 };
 
+typedef struct function_node function_node;
+typedef struct function_hashtable function_hashtable;
 typedef struct identifier_list identifier_list;
 typedef struct identifier_hashtable identifier_hashtable;
 typedef struct function_identifier_node function_identifier_node;
